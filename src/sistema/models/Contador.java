d7dfd1f3a6310e1bcd1810942545a706d50ead89
sara-1504 <sara.delgado-a@uniminuto.edu.co>
2025-11-25 11:41:57 -0500

    codigo documentado

--- src/sistema/models/Contador.java
+++ src/sistema/models/Contador.java
@@ -14,6 +14,7 @@ public abstract class Contador {
 // Dirección física donde está ubicado el contador
     protected String direccion;
 
+    
 /**
      * Constructor que inicializa los datos básicos del contador.
      *
